82a1be858760dec6a79fe40ca70c40bbf856bf1e
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2022-06-20 20:44:23 +0800

    Update UserTabCompletion.java

--- src/main/java/net/tropicbliss/doublelifereloaded/commands/UserTabCompletion.java
+++ src/main/java/net/tropicbliss/doublelifereloaded/commands/UserTabCompletion.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.HumanEntity;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
