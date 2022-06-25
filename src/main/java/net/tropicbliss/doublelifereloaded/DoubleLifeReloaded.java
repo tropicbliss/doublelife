95ef69245a64206a454bf3617dff179f747c01c8
tropicbliss <tropicbliss@protonmail.com>
2022-06-25 15:24:07 +0800

    Add easy tnt recipe

--- src/main/java/net/tropicbliss/doublelifereloaded/DoubleLifeReloaded.java
+++ src/main/java/net/tropicbliss/doublelifereloaded/DoubleLifeReloaded.java
@@ -3,6 +3,11 @@ package net.tropicbliss.doublelifereloaded;
 import net.tropicbliss.doublelifereloaded.commands.*;
 import net.tropicbliss.doublelifereloaded.listeners.GlobalListener;
 import net.tropicbliss.doublelifereloaded.tasks.SelectRandomSoulmate;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 
@@ -21,6 +26,14 @@ public final class DoubleLifeReloaded extends JavaPlugin {
         getCommand("soulbind").setTabCompleter(new UserTabCompletion(List.of(1, 2)));
         getCommand("unbind").setExecutor(new CommandUnbind());
         getCommand("unbind").setTabCompleter(new UserTabCompletion(List.of(1)));
+        ItemStack tnt = new ItemStack(Material.TNT);
+        NamespacedKey easyTnt = new NamespacedKey(this, "easy_tnt");
+        ShapedRecipe recipe = new ShapedRecipe(easyTnt, tnt);
+        recipe.shape("PSP", "SGS", "PSP");
+        recipe.setIngredient('P', Material.PAPER);
+        recipe.setIngredient('S', Material.SAND);
+        recipe.setIngredient('G', Material.GUNPOWDER);
+        Bukkit.addRecipe(recipe);
         getServer().getPluginManager().registerEvents(new GlobalListener(this), this);
     }
 
